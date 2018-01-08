7e2a6df08a8abe14e1ee834a2d76d6b916dc52a1
Oly <hoit@bk.ru>
2018-01-08 13:15:32 +0300

    Create search-tree.hpp

---
+++ include/search-tree.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <memory>
+#include <ostream>
+
+namespace search_trees
+{
+
+template<typename Key, typename Value>
+class SearchTree
+{
+public:
+	virtual ~SearchTree() = default;
+
+	virtual void insert(const Key &key, const Value &value) = 0;
+	virtual void insert(const Key &key, Value &&value) = 0;
+	virtual void insert(Key &&key, const Value &value) = 0;
+	virtual void insert(Key &&key, Value &&value) = 0;
+
+	virtual Value *find(const Key &key) = 0;
+	virtual const Value *find(const Key &key) const = 0;
+
+	virtual Value *min() = 0;
+	virtual const Value *min() const = 0;
+
+	virtual Value *max() = 0;
+	virtual const Value *max() const = 0;
+
+	virtual bool remove(const Key &key) = 0;
+
+	virtual void print(std::ostream &stream) = 0;
+};
+
+template<typename Key, typename Value>
+using SearchTreePtr = std::unique_ptr<SearchTree<Key, Value>>;
+
+} // namespace search_trees
